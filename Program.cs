e6d461bad69609aec44f43966308396ccb553a6d
LCBarradas <100002313+LCBarradas@users.noreply.github.com>
2022-06-08 23:46:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using BE5.Classes;
+
+PessoaFisica novaPf = new PessoaFisica();
+novaPf.nome = "Luiz Carlos";
+Console.WriteLine (novaPf.nome);
+
+
